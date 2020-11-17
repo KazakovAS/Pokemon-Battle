d4a4d5beffdd93f8ca831a842ed0f10186402284
Alexey <admin@kazakov-a.ru>
2020-11-17 23:35:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let firstRow = prompt("Введите 1-ю строку");
-let secondRow = prompt("Введите 2-ю строку");
+const firstRow = prompt("Введите 1-ю строку");
+const secondRow = prompt("Введите 2-ю строку");
 
 let char = prompt("Введите символ, количество которого необходимо посчитать");
 
