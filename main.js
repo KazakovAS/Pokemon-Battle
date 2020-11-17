9b1e109687839d36157f9396507a65ed151ff0f3
Alexey Kazakov <a.kazakov@betaonline.ru>
2020-11-18 00:44:11 +0300

    обновление

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
-let firstRow = prompt("Введите 1-ю строку");
-let secondRow = prompt("Введите 2-ю строку");
+const firstRow = prompt("Введите 1-ю строку");
+const secondRow = prompt("Введите 2-ю строку");
 
-let char = prompt("Введите символ, количество которого необходимо посчитать");
+const char = prompt("Введите символ, количество которого необходимо посчитать");
 
-let resultFirstRow = 0;
-let resultSecondRow = 0;
-
-function getRow(firstRow, secondRow) {
-    for (let i = 0; i < firstRow.length; i++) {
-        let charString = firstRow.charAt(i);
-
-        if (charString === char) {
-            resultFirstRow += 1;
-        }
-    }
-
-    for (let i = 0; i < secondRow.length; i++) {
-        let charString = secondRow.charAt(i);
-
-        if (charString === char) {
-            resultSecondRow += 1;
-        }
-    }
+function getRow(firstRow, secondRow, char) {
+    const resultFirstRow  = countCharInRow(firstRow, char);
+    const resultSecondRow = countCharInRow(secondRow, char);
 
     if (resultFirstRow > resultSecondRow) {
         alert(`Количество символов ${char} больше в 1-ой строке`);
-    } else {
+    } else if (resultFirstRow < resultSecondRow) {
         alert(`Количество символов ${char} больше во 2-ой строке`);
+    } else {
+        alert(`Количество символов ${char} одинаково в обех строках`)
     }
 }
 
-getRow(firstRow, secondRow);
+function countCharInRow(row, char) {
+    let cnt = 0;
+
+    for (let i = 0; i < row.length; i++) {
+        if (row[i] === char) {
+            console.log(row[i])
+            cnt += 1;
+        }
+    }
+    return cnt;
+}
+
+getRow(firstRow, secondRow, char);
